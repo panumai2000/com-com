cb9b25b9dce70d8ea150dd7616cd5c67a3b3354c
panumai2000 <81949157+panumai2000@users.noreply.github.com>
2021-05-19 14:45:57 +0900

    マンガ登録機能の実施

---
+++ app/models/genre.rb
@@ -0,0 +1,18 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'バトル' },
+    { id: 3, name: 'SF' },
+    { id: 4, name: 'ギャグ' },
+    { id: 5, name: 'サスペンス' },
+    { id: 6, name: 'ヤンキー' },
+    { id: 7, name: 'スポーツ' },
+    { id: 8, name: 'ラブコメ' },
+    { id: 9, name: '少女漫画' },
+    { id: 10, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :comics
+
+  end
---
+++ app/models/magazine.rb
@@ -0,0 +1,23 @@
+class Magazine < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '週刊少年ジャンプ' },
+    { id: 3, name: 'ジャンプSQ' },
+    { id: 4, name: 'ヤングジャンプ' },
+    { id: 5, name: 'グランドジャンプ' },
+    { id: 6, name: 'ウルトラジャンプ' },
+    { id: 7, name: '週刊少年マガジン' },
+    { id: 8, name: 'ヤングマガジン' },
+    { id: 9, name: 'モーニング' },
+    { id: 10, name: '週刊少年チャンピオン' },
+    { id: 11, name: 'ヤングチャンピオン' },
+    { id: 12, name: '少年サンデー' },
+    { id: 13, name: 'ビッグスピリッツ' },
+    { id: 14, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :comics
+
+  end
+
