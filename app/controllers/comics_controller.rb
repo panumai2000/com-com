9b822561caea406e2635b2a5a10ecd4d6196953a
panumai2000 <81949157+panumai2000@users.noreply.github.com>
2021-05-21 19:43:25 +0900

    登録したマンガの編集、削除機能の実装

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,6 @@
 class ComicsController < ApplicationController
+  before_action :set_comic, only: [:show, :edit, :update, :destroy]
+
   def index
     @comics = Comic.all
   end
@@ -16,9 +18,35 @@ class ComicsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def destroy
+    if @comic.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
+  def update
+    if @comic.update(comic_params)
+      redirect_to comic_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
   private
   def comic_params
     params.require(:comic).permit(:image, :name, :genre_id, :magazine_id, :author, :number, :end).merge(user_id: current_user.id)
+  end
+
+  def set_comic
+    @comic = Comic.find(params[:id])
   end
 
 end
