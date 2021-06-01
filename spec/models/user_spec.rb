76bb50bb9cce14e171080217f828b2d1f39241dd
panumai2000 <81949157+panumai2000@users.noreply.github.com>
2021-06-01 20:55:31 +0900

    ニックネーム、メールのテストコードを記入した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe "ユーザー新規登録" do
     it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-      user.valid?
+      @user.nickname = ''
+      @user.valid?
     end
     it "emailが空では登録できない" do
+      @user.email = ''
+      @user.valid?
     end
   end
 end
